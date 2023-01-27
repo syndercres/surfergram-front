a60a8a658d93e8c6b6adb0ed0b9b740721b82feb
syndercombecreswell <syndercombecreswell@gmail.com>
2023-01-27 10:07:37 +0000

    starting to impliment routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import MainPage from "./components/MainPage";
 import CommentPage, { Ispot } from "./components/CommentPage";
 import AddASpot from "./components/AddASpot";
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App(): JSX.Element {
-  const [pageView, setPageView] = useState<"comment" | "main"| "add-spot">("main");
-  const [displaySpot, setDisplaySpot] = useState<Ispot>({spot_id:1, name:"no spot", description:"",rating:0,directions:"" });
+  const [pageView, setPageView] = useState<"comment" | "main" | "add-spot">(
+    "main"
+  );
+  const [displaySpot, setDisplaySpot] = useState<Ispot>({
+    spot_id: 1,
+    name: "no spot",
+    description: "",
+    rating: 0,
+    directions: "",
+  });
   function selectSpot(selectedDisplaySpot: Ispot) {
     setDisplaySpot(selectedDisplaySpot);
     setPageView("comment");
@@ -19,40 +27,28 @@ function App(): JSX.Element {
   }
   return (
     <>
-    <BrowserRouter>
       <Routes>
-          <Route path="/" element={<NavBar/>}/>
-          <Route index element={<MainPage handleChangeSpotId={selectSpot} />} />
-          <Route path="comments/:id" element={<CommentPage displaySpot={displaySpot} handleReturnMain={backToSpots} />} />
-          <Route path="/add-spot" element={<AddASpot />} />
-
+        <Route path="/" element={<NavBar />} />
+        <Route index element={<MainPage handleChangeSpotId={selectSpot} />} />
+        <Route
+          path="comments/:id"
+          element={
+            <CommentPage
+              displaySpot={displaySpot}
+              handleReturnMain={backToSpots}
+            />
+          }
+        />
+        <Route path="/add-spot" element={<AddASpot />} />
       </Routes>
-    </BrowserRouter>
 
       <div className="header-buttons">
         <button onClick={backToSpots}>spots</button>
-        <button onClick={()=> setPageView("add-spot")}>add a spot</button>
-      </div>
-      <div>
-        {pageView === "main" ? (
-          <MainPage handleChangeSpotId={selectSpot} />
-        ) : (
-          <></>
-        )}
-      </div>
-      <div>
-        {pageView === "comment" ? (
-          <CommentPage displaySpot={displaySpot} handleReturnMain={backToSpots} />
-        ) : (
-          <></>
-        )}
-      </div>
-      <div>
-        {pageView === "add-spot" ? (
-          <AddASpot />
-        ) : (
-          <></>
-        )}
+        <button>
+          <NavLink to="/add-spot" className="navbarAddresource navitem">
+            add a spot
+          </NavLink>
+        </button>
       </div>
     </>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
+import { NavLink } from "react-router-dom";
 
-
-
-export default function NavBar():JSX.Element{
-
-    return(
-        <div>
-            <h1>nav time!</h1>
-        </div>
-    )
+export default function NavBar(): JSX.Element {
+  return (
+    <div>
+      <h1>nav time!</h1>
+      <NavLink to="/add-spot" className="navbarAddresource navitem">
+        Add Resource!
+      </NavLink>
+    </div>
+  );
 }
