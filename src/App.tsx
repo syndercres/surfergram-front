16fe47f8c3ac8d36a5bef75803622a7265da2034
syndercombecreswell <syndercombecreswell@gmail.com>
2023-02-16 23:14:44 +0000

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import MainPage from "./components/MainPage";
 import CommentPage from "./components/CommentPage";
 import AddASpot from "./components/AddASpot";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App(): JSX.Element {
   return (
     <>
-      <div className="header-buttons">
-        <NavLink to="">
-          <button>spots</button>
-        </NavLink>
+      <NavBar />
 
-        <NavLink to="/add-spot">
-          <button>add a spot </button>
-        </NavLink>
-      </div>
       <Routes>
         <Route path="/" element={<NavBar />} />
         <Route index element={<MainPage />} />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,21 @@
 import { NavLink } from "react-router-dom";
+import "./Nav.css";
 
 export default function NavBar(): JSX.Element {
   return (
     <div>
-      <h1>nav time!</h1>
-      <button></button>
-      <NavLink to="/add-spot" className="navbarAddresource navitem">
-        Add Resource!
-      </NavLink>
+      <div className="nav-container">
+        <h1>Surfergram</h1>
+        <div className="nav-links">
+          <NavLink to="">
+            <button className="nav-button">spots</button>
+          </NavLink>
+
+          <NavLink to="/add-spot">
+            <button className="nav-button">add a spot </button>
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
